225afa6645e25aa1378b8f05ee6d4cfa8f0d4b77
Gary Kovar <gkovar@gmail.com>
2017-06-13 21:07:19 -0400

    Adds a method to send a message

--- includes/class-slack-post.php
+++ includes/class-slack-post.php
@@ -22,6 +22,24 @@ class WPMD_Slack_Post {
 	protected $plugin = null;
 
 	/**
+	 * The message we are sending to slack.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @var string
+	 */
+	protected $message = null;
+
+	/**
+	 * The slack endpoint.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @var string
+	 */
+	protected $slack_endpoint = null;
+
+	/**
 	 * Constructor.
 	 *
 	 * @since  0.1.0
@@ -39,6 +57,39 @@ class WPMD_Slack_Post {
 	 * @since  0.1.0
 	 */
 	public function hooks() {
-
+		add_action( 'init', [ $this, 'get_slack_endpoint'] );
 	}
+
+	/**
+	 * Get the slack endpoint.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @author Gary Kovar
+	 */
+	public function get_slack_endpoint(){
+		$doorbell_options = get_option( 'wp_meetup_doorbell_options' );
+		$this->slack_endpoint = $doorbell_options[ 'slack_endpoint' ];
+	}
+
+	/**
+	 * Send the message to slack.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @author Gary Kovar
+	 */
+	public function sned_message() {
+		wp_remote_post( $this->slack_endpoint, array(
+				'method' => 'POST',
+				'timeout' => 30,
+				'redirection' => 5,
+				'httpversion' => '1.0',
+				'blocking' => true,
+				'body' => $this->message
+			)
+		);
+	}
+
+
 }
