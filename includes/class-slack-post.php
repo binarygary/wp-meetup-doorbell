21c5f48030fb067cf4f4939e5bd6217236809009
Gary Kovar <gkovar@gmail.com>
2017-06-13 21:43:27 -0400

    modifies how we encode the message

--- includes/class-slack-post.php
+++ includes/class-slack-post.php
@@ -57,40 +57,40 @@ class WPMD_Slack_Post {
 	 * @since  0.1.0
 	 */
 	public function hooks() {
-		add_action( 'init', [ $this, 'get_slack_endpoint'] );
+		add_action( 'init', [ $this, 'get_slack_endpoint' ] );
 	}
 
 	/**
 	 * Get the slack endpoint.
 	 *
-	 * @since 0.1.0
+	 * @since  0.1.0
 	 *
 	 * @author Gary Kovar
 	 */
-	public function get_slack_endpoint(){
-		$doorbell_options = get_option( 'wp_meetup_doorbell_options' );
-		$this->slack_endpoint = $doorbell_options[ 'slack_endpoint' ];
+	public function get_slack_endpoint() {
+		$doorbell_options     = get_option( 'wp_meetup_doorbell_options' );
+		$this->slack_endpoint = $doorbell_options['slack_endpoint'];
 	}
 
 	/**
 	 * Send the message to slack.
 	 *
-	 * @since 0.1.0
+	 * @since  0.1.0
 	 *
 	 * @author Gary Kovar
 	 */
 	public function send_message() {
 		$message_results = wp_remote_post( $this->slack_endpoint, array(
-				'method' => 'POST',
-				'timeout' => 30,
+				'method'      => 'POST',
+				'timeout'     => 30,
 				'redirection' => 5,
 				'httpversion' => '1.0',
-				'blocking' => true,
-				'body' => $this->message
+				'blocking'    => true,
+				'body'        => $this->message,
 			)
 		);
 
-		error_log( print_r( $message_results,1 ) );
+		error_log( print_r( $message_results, 1 ) );
 	}
 
 	/**
@@ -98,12 +98,17 @@ class WPMD_Slack_Post {
 	 *
 	 * @author Gary Kovar
 	 *
-	 * @since 0.1.0
+	 * @since  0.1.0
 	 *
 	 * @param string $message Theoretically the message the user wants to send in.
 	 */
 	public function set_message( $message ) {
-		$this->message = json_encode( sanitize_textarea_field( $message ) );
+		$this->message = array(
+			'payload' => json_encode( array(
+					'text' => sanitize_textarea_field( $message ),
+				)
+			),
+		);
 	}
 
 }
