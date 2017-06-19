51baea7755ec15565050a61b1cfb721d268e8b18
Gary Kovar <gkovar@gmail.com>
2017-06-18 21:23:52 -0400

    Unburns the author that is not familiar with namespacing

--- wp-meetup-doorbell.php
+++ wp-meetup-doorbell.php
@@ -1,7 +1,5 @@
 <?php
 
-namespace WDS\MeetupDoorbell;
-
 /**
  * Plugin Name: WP Meetup Doorbell
  * Plugin URI:  https://wpjax.org
@@ -47,16 +45,15 @@ namespace WDS\MeetupDoorbell;
  * @since  0.1.0
  * @param  string $class_name Name of the class being requested.
  */
-function wp_meetup_doorbell_autoload_classes( $class_name ) {
 
+
+function wp_meetup_doorbell_autoload_classes( $class_name ) {
 	// If our class doesn't have our prefix, don't load it.
 	if ( 0 !== strpos( $class_name, 'WPMD_' ) ) {
 		return;
 	}
-
 	// Set up our filename.
 	$filename = strtolower( str_replace( '_', '-', substr( $class_name, strlen( 'WPMD_' ) ) ) );
-
 	// Include our file.
 	WP_Meetup_Doorbell::include_file( 'includes/class-' . $filename );
 }
@@ -184,7 +181,7 @@ final class WP_Meetup_Doorbell {
 		$this->slack_post = new WPMD_Slack_Post( $this );
 		$this->options = new WPMD_Options( $this );
 		$this->twilio_endpoint = new WPMD_Twilio_Endpoint( $this );
-		$this->meetup_event = new WPMD_Meetup_Event( $this );
+		//$this->meetup_event = new WPMD_Meetup_Event( $this );
 	} // END OF PLUGIN CLASSES FUNCTION
 
 	/**
@@ -343,7 +340,7 @@ final class WP_Meetup_Doorbell {
 			case 'slack_post':
 			case 'options':
 			case 'twilio_endpoint':
-			case 'meetup_event':
+			//case 'meetup_event':
 				return $this->$field;
 			default:
 				throw new Exception( 'Invalid ' . __CLASS__ . ' property: ' . $field );
