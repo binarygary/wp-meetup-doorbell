8801c1e48a83b729e60531679a372c56ba9f8c11
Gary Kovar <gkovar@gmail.com>
2017-06-18 21:39:49 -0400

    Adds a class to retrieve calendar data

---
+++ includes/class-meetup-calendar.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * WP Meetup Doorbell Meetup Calendar.
+ *
+ * @since   0.1.0
+ * @package WP_Meetup_Doorbell
+ */
+
+/**
+ * WP Meetup Doorbell Meetup Calendar.
+ *
+ * @since 0.1.0
+ */
+class WPMD_Meetup_Calendar {
+	
+
+	/**
+	 * Constructor.
+	 *
+	 * @since  0.1.0
+	 */
+	public function __construct() {
+
+	}
+}
